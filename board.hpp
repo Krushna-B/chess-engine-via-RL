b08d3ab15666cdeaa097f1cab1169ed58309df09
Krushna-B <Krushna.Bhanushali@unc.edu>
2026-08-01 00:54:07 -0400

    Add magic number search for hashing occupancy bitboards

--- board.hpp
+++ board.hpp
@@ -5,6 +5,7 @@
 
 #include <iostream>
 #include <ostream>
+#include <random>
 
 // Aliaes
 using Bitboard = uint64_t;
@@ -204,6 +205,9 @@ Magic Bitboards
 ---------------------------
  */
 
+constexpr int MAX_BISHOP_OCCUPANCIES = 1 << 9;
+constexpr int MAX_ROOK_OCCUPANCIES = 1 << 12;
+
 // Set occupany builder
 constexpr Bitboard set_occupancy(int idx, int bits_in_mask,
                                  Bitboard attack_mask) {
@@ -222,11 +226,23 @@ constexpr Bitboard set_occupancy(int idx, int bits_in_mask,
   return occupancy;
 }
 
+/**
+Magic index generation
+ */
+constexpr std::size_t magic_index(Bitboard occupancy, Bitboard magic,
+                                  int relevant_bits) {
+  return static_cast<std::size_t>((occupancy * magic) >> (64 - relevant_bits));
+}
+// Not crytographically secure, psuedo RNG
+Bitboard random_u64() {
+  // Seeded via hardware entropy
+  std::mt19937 engine{std::random_device{}()};
+  return engine();
+}
+
 /***
 Bishop LUT
 */
-constexpr int MAX_BISHOP_OCCUPANCIES = 1 << 9;
-
 /**
 Helper mask for bishop at each position
  */
@@ -305,29 +321,56 @@ constexpr Bitboard bishop_attacks_on_the_fly(int square, Bitboard blockers) {
   }
   return attacks;
 }
+constexpr Bitboard find_bishop_magic(int square) {
+  const Bitboard attack_mask = mask_bishop_attcks(square);
+  const int relevant_bits = std::popcount(attack_mask);
+  const int occupancy_count = 1 << relevant_bits;
 
-/**
-Magic index generation
- */
-constexpr std::size_t magic_index(Bitboard occupancy, Bitboard magic,
-                                  int relevant_bits) {
-  return static_cast<std::size_t>((occupancy * magic) >> (64 - relevant_bits));
+  // Store all occupancies and all attacks
+  std::array<Bitboard, MAX_BISHOP_OCCUPANCIES> occupancies{};
+  std::array<Bitboard, MAX_BISHOP_OCCUPANCIES> attacks{};
+
+  // For each occupancy generate that occupany and it's attack
+  for (int idx{}; idx < occupancy_count; idx++) {
+    occupancies[idx] = set_occupancy(idx, relevant_bits, attack_mask);
+    attacks[idx] = bishop_attacks_on_the_fly(square, occupancies[idx]);
+  }
+
+  // Find magic number candidate via trial and error
+  while (true) {
+    const Bitboard magic_num = random_u64();
+
+    std::array<Bitboard, MAX_BISHOP_OCCUPANCIES> used_attacks{};
+    std::array<bool, MAX_BISHOP_OCCUPANCIES> filled_occupanies{};
+    bool failed = false;
+
+    for (int idx{}; idx < occupancy_count; idx++) {
+      auto hash_index = magic_index(occupancies[idx], magic_num, relevant_bits);
+      if (!filled_occupanies[hash_index]) {
+        filled_occupanies[hash_index] = true;
+        used_attacks[hash_index] = attacks[index];
+      } else if (used_attacks[hash_index] != attacks[idx]) {
+        failed = true;
+        break;
+      }
+    }
+    if (!failed) {
+      return magic_num;
+    }
+  }
+}
+// Magic number's for every single square a1,......h8
+constexpr std::array<Bitboard, 64> generate_all_bishop_magics() {
+  std::array<Bitboard, 64> magics{};
+  for (int square{}; square < 64; square++) {
+    magics[square] = find_bishop_magic(square);
+  }
+  return magics
 }
 
 consteval std::array<Bitboard, 64> generateBishopLUT() {
   std::array<Bitboard, 64> lut{};
   for (int square{}; square < 64; square++) {
-    const Bitboard attack_mask = mask_bishop_attcks(square);
-    const int relevant_bits = std::popcount(attack_mask);
-    const int occupancy_count = 1 << relevant_bits;
-
-    std::array<Bitboard, MAX_BISHOP_OCCUPANCIES> occupancies{};
-    std::array<Bitboard, MAX_BISHOP_OCCUPANCIES> attacks{};
-
-    for (int idx{}; idx < occupancy_count; idx++) {
-      occupancies[idx] = set_occupancy(idx, relevant_bits, attack_mask);
-      attacks[idx] = bishop_attacks_on_the_fly(square, occupancies[idx]);
-    }
   }
 
   return lut;
